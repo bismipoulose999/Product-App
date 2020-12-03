37b38eeea4d1f4375d4f4d6c1cfa203a72467bd1
bismipoulose999 <bismipoulose999@gmail.com>
2020-12-03 17:26:52 +0530

    css added

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>
-    <h1>Product App</h1>
+<body class="color">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Product App</h1>
     <table>
         <tr>
             <td>Product Name</td>
@@ -30,5 +35,8 @@
             <td><input type="text"></td>
         </tr>
     </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
